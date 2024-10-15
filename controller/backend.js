c0c5c0815bf979dd6e0887fd46d202f60a960ea2
Anas-aoude <61455217+Anas-aoude@users.noreply.github.com>
2024-10-15 11:55:35 +0200

    Update backend.js

---
+++ controller/backend.js
@@ -0,0 +1,10 @@
+const User = require('../models/user.js');
+const Transaction = require('../models/transaction.js');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+
+exports.backenLogin = async function (req, res) {
+// DB abfragen 
+  };
+
