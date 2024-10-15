f08cb5f7772240d56d6cfa4967bb2b2427dd2863
Anas-aoude <61455217+Anas-aoude@users.noreply.github.com>
2024-10-15 12:13:00 +0200

    Update backend.js

--- controller/backend.js
+++ controller/backend.js
@@ -4,7 +4,27 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 
 
-exports.backenLogin = async function (req, res) {
+exports.backendLogin = async function (req, res) {
 // DB abfragen 
+ const access = await User.findOne({ username: req.body.username, password:req.body.password  });
+    if (!access || access.role !== 'admin') {
+      return res.redirect('/');
+    }
+  const insertInTrans = new Transaction({
+      action: 'Backend',
+    });
+await insertInTrans.save();
+
+if (access.role === 'admin') {
+      req.session.permission = 'admin';
+      req.session.user = access;
+      res.redirect('/backend')
+    } else {
+      res.redirect('/');
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect('/'); // Bei einem Fehler ebenfalls umleiten
+  }
   };
 
