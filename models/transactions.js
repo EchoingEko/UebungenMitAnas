e9215df1e834f092b8720a9591d589fb8f8c0fc4
Anas-aoude <61455217+Anas-aoude@users.noreply.github.com>
2024-10-15 11:36:23 +0200

    Update transactions.js

--- models/transactions.js
+++ models/transactions.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+var TransactionSchema = new Schema({
+    action                : {type: String, default:"account"}, // account, file, photo, device 
+    ts                    : { type: Date, default: Date.now },
+    login                 : String,
+  });
+
+module.exports = mongoose.model('Transaction',TransactionSchema);
